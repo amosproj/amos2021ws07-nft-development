ee78df150d759cd436a26f391b501d85184aaada
dominic1608 <mail@d-heil.de>
2021-11-02 05:34:54 +0100

    Added eslint for automatic linting; added license headers to all files; added development section to readme with commands for linting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-						<header className="App-header">
+							<header className="App-header">
 				<p>
 					Coming soon!
 				</p>
