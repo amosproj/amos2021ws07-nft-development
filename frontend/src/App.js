8403312fdd2f3d9f46d8fd186d02491cdc93c9ee
dominic1608 <mail@d-heil.de>
2021-11-03 02:30:26 +0100

    Adjusted ESLint workflow to lint on push and pull request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-								<p>
+				<p>
 					Coming soon!
 				</p>
 				<img src={logo} className="App-logo" alt="logo" />
