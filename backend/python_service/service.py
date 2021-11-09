46e4c0da2696251dc381a5e141817d1b4a26bffc
Felix Steinkohl <steinkohl@campus.tu-berlin.de>
2021-11-09 21:51:48 +0100

    altered example

--- backend/python_service/service.py
+++ backend/python_service/service.py
@@ -1,3 +1,3 @@
 def hello_world():
-    return 'hello world'
+    return "hello world"
 
--- backend/python_service/test/test_service.py
+++ backend/python_service/test/test_service.py
@@ -5,7 +5,7 @@ from backend.python_service.service import hello_world
 class MainTest(unittest.TestCase):
 
     def test_hello_world(self):
-        self.assertEqual(hello_world(), 'hello world')
+        self.assertEqual(hello_world(), "hello world")
 
 
 if __name__ == "__main__":
