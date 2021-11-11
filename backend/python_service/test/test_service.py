7dae363fddf3b4c303a26f4cf299d7313fcb83d1
Felix Steinkohl <steinkohl@campus.tu-berlin.de>
2021-11-11 12:43:53 +0100

    test commit - messed up format

--- backend/python_service/test/test_service.py
+++ backend/python_service/test/test_service.py
@@ -5,6 +5,7 @@ import unittest
 from backend.python_service.service import hello_world
 
 
+
 class MainTest(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), 'hello world')
