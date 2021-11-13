bbe1a7195f90e3e12d1888db726af7f58618c148
Nick <nickstenderst@gmail.com>
2021-11-13 13:39:59 +0100

    init backend-connect-wallet

---
+++ backend/appwrite-functions/set-wallet/main.py
@@ -0,0 +1,39 @@
+import os
+import json
+from appwrite.client import Client
+from appwrite.services.database import Database
+
+# provided by default
+USER_ID = os.environ.get('APPWRITE_FUNCTION_USER_ID')
+PAYLOAD = os.environ.get('APPWRITE_FUNCTION_DATA')
+APPWRITE_PROJECT = os.environ.get('APPWRITE_FUNCTION_PROJECT_ID')
+
+# provided by cloud function env vars
+APPWRITE_ENDPOINT = os.environ.get('APPWRITE_ENDPOINT')
+APPWRITE_API_KEY = os.environ.get('APPWRITE_API_KEY')
+WALLETS_COLLECTION_ID = os.environ.get('WALLETS_COLLECTION_ID')
+
+client = Client()
+database = Database(client)
+
+(client
+ .set_endpoint(APPWRITE_ENDPOINT)
+ .set_project(APPWRITE_PROJECT)
+ .set_key(APPWRITE_API_KEY)
+ )
+
+desiredWalletAddress = json.loads(PAYLOAD)["walletAddress"]
+
+readResult = database.list_documents(WALLETS_COLLECTION_ID, ["userId=" + USER_ID])["documents"]
+
+if len(readResult) > 0:
+    writeResult = database.update_document(WALLETS_COLLECTION_ID, readResult[0]["$id"],
+                                           {"walletAddress": desiredWalletAddress})
+else:
+    writeResult = database.create_document(WALLETS_COLLECTION_ID, {"userId": USER_ID, "walletAddress": desiredWalletAddress})
+
+res = {
+    "status": "success",
+    "dbResult": writeResult
+}
+print(json.dumps(res))
