5fd57f5a2a8b0d138f84e665ec5efa29ace86005
Nick <nickstenderst@gmail.com>
2021-11-13 18:19:17 +0100

    fixed formatting and added rule to collection, so that members can use it.

--- backend/appwrite-functions/set-wallet/main.py
+++ backend/appwrite-functions/set-wallet/main.py
@@ -4,36 +4,42 @@ from appwrite.client import Client
 from appwrite.services.database import Database
 
 # provided by default
-USER_ID = os.environ.get('APPWRITE_FUNCTION_USER_ID')
-PAYLOAD = os.environ.get('APPWRITE_FUNCTION_DATA')
-APPWRITE_PROJECT = os.environ.get('APPWRITE_FUNCTION_PROJECT_ID')
+USER_ID = os.environ.get("APPWRITE_FUNCTION_USER_ID")
+PAYLOAD = os.environ.get("APPWRITE_FUNCTION_DATA")
+APPWRITE_PROJECT = os.environ.get("APPWRITE_FUNCTION_PROJECT_ID")
 
 # provided by cloud function env vars
-APPWRITE_ENDPOINT = os.environ.get('APPWRITE_ENDPOINT')
-APPWRITE_API_KEY = os.environ.get('APPWRITE_API_KEY')
-WALLETS_COLLECTION_ID = os.environ.get('WALLETS_COLLECTION_ID')
+APPWRITE_ENDPOINT = os.environ.get("APPWRITE_ENDPOINT")
+APPWRITE_API_KEY = os.environ.get("APPWRITE_API_KEY")
+WALLETS_COLLECTION_ID = os.environ.get("WALLETS_COLLECTION_ID")
 
 client = Client()
 database = Database(client)
 
-(client
- .set_endpoint(APPWRITE_ENDPOINT)
- .set_project(APPWRITE_PROJECT)
- .set_key(APPWRITE_API_KEY)
- )
+(
+    client.set_endpoint(APPWRITE_ENDPOINT)
+    .set_project(APPWRITE_PROJECT)
+    .set_key(APPWRITE_API_KEY)
+)
 
 desiredWalletAddress = json.loads(PAYLOAD)["walletAddress"]
 
-readResult = database.list_documents(WALLETS_COLLECTION_ID, ["userId=" + USER_ID])["documents"]
+readResult = database.list_documents(WALLETS_COLLECTION_ID, ["userId=" + USER_ID])[
+    "documents"
+]
 
 if len(readResult) > 0:
-    writeResult = database.update_document(WALLETS_COLLECTION_ID, readResult[0]["$id"],
-                                           {"walletAddress": desiredWalletAddress})
+    writeResult = database.update_document(
+        WALLETS_COLLECTION_ID,
+        readResult[0]["$id"],
+        {"walletAddress": desiredWalletAddress},
+    )
 else:
-    writeResult = database.create_document(WALLETS_COLLECTION_ID, {"userId": USER_ID, "walletAddress": desiredWalletAddress}, ["user:" + USER_ID])
+    writeResult = database.create_document(
+        WALLETS_COLLECTION_ID,
+        {"userId": USER_ID, "walletAddress": desiredWalletAddress},
+        ["user:" + USER_ID],
+    )
 
-res = {
-    "status": "success",
-    "dbResult": writeResult
-}
+res = {"status": "success", "dbResult": writeResult}
 print(json.dumps(res))
