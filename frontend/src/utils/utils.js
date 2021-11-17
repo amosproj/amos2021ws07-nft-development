8994fae4c6f4810468c95acf7c5c080a88929a41
dominic1608 <mail@d-heil.de>
2021-11-17 10:41:07 +0100

    Added regex source as comment

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: 2021 Dominic Heil <d.heil@campus.tu-berlin.de>
 
+// regex source: https://stackoverflow.com/a/46181/7838440
 export const isValidEmail = (email) => {
 	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
