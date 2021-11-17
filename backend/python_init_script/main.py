ef5c2fc4739e437a04917844f864b24f8f0f29fd
Felix Steinkohl <steinkohl@campus.tu-berlin.de>
2021-11-17 20:04:29 +0100

    updated init-script

---
+++ backend/python_init_script/main.py
@@ -0,0 +1,45 @@
+import os
+from appwrite.client import Client
+
+# from appwrite.services.database import Database
+from appwrite.services.users import Users
+from appwrite.services.teams import Teams
+
+# from appwrite.services.functions import Functions
+
+
+def init_client(
+    project_id: str = os.environ.get("APPWRITE_FUNCTION_PROJECT_ID"),
+    endpoint: str = os.environ.get("APPWRITE_ENDPOINT"),
+    api_key: str = os.environ.get("APPWRITE_API_KEY"),
+) -> Client:
+    _client = Client()
+    _client.set_endpoint(endpoint)
+    _client.set_project(project_id)
+    _client.set_key(api_key)
+    return _client
+
+
+client = init_client()
+users = Users(client)
+teams = Teams(client)
+# database = Database(client)
+# funs = Functions(client)
+
+result = teams.create("Admins")
+_id = result["$id"]
+_endpoint = os.environ.get("APPWRITE_ENDPOINT")
+
+# fmt: off
+teams.create_membership(team_id=_id, email="admin0@admin1.de", roles=['owner'], url=_endpoint, name="Berinike")
+teams.create_membership(team_id=_id, email="admin1@admin1.de", roles=['owner'], url=_endpoint, name="Dominic")
+teams.create_membership(team_id=_id, email="admin2@admin1.de", roles=['owner'], url=_endpoint, name="Felix")
+teams.create_membership(team_id=_id, email="admin3@admin1.de", roles=['owner'], url=_endpoint, name="Nick S.")
+teams.create_membership(team_id=_id, email="admin4@admin1.de", roles=['owner'], url=_endpoint, name="Christoph")
+teams.create_membership(team_id=_id, email="admin5@admin1.de", roles=['owner'], url=_endpoint, name="Ba Que")
+teams.create_membership(team_id=_id, email="admin6@admin1.de", roles=['owner'], url=_endpoint, name="Nik N.")
+teams.create_membership(team_id=_id, email="admin7@admin1.de", roles=['owner'], url=_endpoint, name="Jannis")
+# fmt: on
+
+result = users.list()
+print(result)
