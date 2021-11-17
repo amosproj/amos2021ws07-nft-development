4b2bc3bb934f3fde09221fcf3ad3fbc9a3e05d1c
autoblack <cclauss@users.noreply.github.com>
2021-11-17 19:05:27 +0000

    fixup: Format Python code with Black

--- backend/appwrite-functions/announcement/ancm-function/main.py
+++ backend/appwrite-functions/announcement/ancm-function/main.py
@@ -17,17 +17,16 @@ def init_client():
 
     return client
 
+
 def print_failure_message(message: str):
-    print(json.dumps({
-        "status": "failure",
-        "message": message
-    }))
+    print(json.dumps({"status": "failure", "message": message}))
+
 
 def main():
     client = init_client()
     database = Database(client)
     PAYLOAD = os.environ.get("APPWRITE_FUNCTION_DATA")
-    if (not PAYLOAD):
+    if not PAYLOAD:
         exit()
 
     try:
@@ -48,7 +47,7 @@ def main():
             print("'Announcements' collection not found!", file=sys.stderr)
             exit()
 
-        if (client_payload["action"] == "getAnnouncements"):
+        if client_payload["action"] == "getAnnouncements":
             # Extract data from payload
             nbr_ancm = client_payload["numberOfAnnouncements"]
             timestamp = client_payload["timestamp"]
@@ -58,79 +57,91 @@ def main():
                 "action": "getAnnouncements",
                 "status": "success",
                 "sum": -1,
-                "announcements": []
+                "announcements": [],
             }
             # # Get "Announcements" collection ID
             # listCollection = database.list_collections()
             # for collection in listCollection["collections"]:
             #     if collection["name"] == "Announcements":
-                    # Query data from collection
+            # Query data from collection
             listDocuments = database.list_documents(
-                    collection_id=collection_id,#collection["$id"],
-                    order_field="created_at",
-                    order_type="DESC",
-                    filters= [f"created_at>={timestamp}"] if after else [f"created_at<={timestamp}"],
-                    limit=nbr_ancm
-                )
+                collection_id=collection_id,  # collection["$id"],
+                order_field="created_at",
+                order_type="DESC",
+                filters=[f"created_at>={timestamp}"]
+                if after
+                else [f"created_at<={timestamp}"],
+                limit=nbr_ancm,
+            )
             # Add data to payload
             return_payload["sum"] = len(listDocuments["documents"])
             for document in listDocuments["documents"]:
-                return_payload["announcements"].append({
-                    "created_at": document["created_at"],
-                    "updated_at": document["updated_at"],
-                    "title": document["title"],
-                    "content": document["content"]
-                })
+                return_payload["announcements"].append(
+                    {
+                        "created_at": document["created_at"],
+                        "updated_at": document["updated_at"],
+                        "title": document["title"],
+                        "content": document["content"],
+                    }
+                )
             # There is no way to return JSON data back directly to client, so we transfer by using stdout.
             print(json.dumps(return_payload))
             exit()
 
-        elif (client_payload["action"] == "addAnnouncements"):
+        elif client_payload["action"] == "addAnnouncements":
             announcements = client_payload["announcements"]
             # listCollection = database.list_collections()
             # for collection in listCollection["collections"]:
             #     if collection["name"] == "Announcements":
             for ancm in announcements:
                 createDocumentResult = database.create_document(
-                    collection_id=collection_id,#collection["$id"],
+                    collection_id=collection_id,  # collection["$id"],
                     data={
                         "created_at": time.time(),
                         "updated_at": time.time(),
                         "title": ancm["title"],
-                        "content": ancm["content"]
-                    }
+                        "content": ancm["content"],
+                    },
                 )
             # Success message
-            print(json.dumps({
-                "action": "addAnnouncements",
-                "status": "success",
-                "sum": len(announcements)
-            }))
-        elif (client_payload["action"] == "removeAnnouncements"):
+            print(
+                json.dumps(
+                    {
+                        "action": "addAnnouncements",
+                        "status": "success",
+                        "sum": len(announcements),
+                    }
+                )
+            )
+        elif client_payload["action"] == "removeAnnouncements":
             anmc_created_dates = client_payload["created_dates"]
             removed = []
             for anmc_date in anmc_created_dates:
                 listDocuments = database.list_documents(
-                    collection_id=collection_id,#collection["$id"],
+                    collection_id=collection_id,  # collection["$id"],
                     order_field="created_at",
-                    filters= [f"created_at={anmc_date}"],
-                    limit=1
+                    filters=[f"created_at={anmc_date}"],
+                    limit=1,
                 )
                 # Remove the document
-                if (len(listDocuments["documents"]) == 1):
+                if len(listDocuments["documents"]) == 1:
                     database.delete_document(
                         collection_id=collection_id,
-                        document_id=listDocuments["documents"][0]["$id"]
+                        document_id=listDocuments["documents"][0]["$id"],
                     )
                     removed.append(anmc_date)
             # Success message
-            print(json.dumps({
-                "action": "removeAnnouncements",
-                "status": "success",
-                "sum": len(removed),
-                "removed": removed
-            }))
-    except Exception as e: 
+            print(
+                json.dumps(
+                    {
+                        "action": "removeAnnouncements",
+                        "status": "success",
+                        "sum": len(removed),
+                        "removed": removed,
+                    }
+                )
+            )
+    except Exception as e:
         traceback.print_exc()
 
 
