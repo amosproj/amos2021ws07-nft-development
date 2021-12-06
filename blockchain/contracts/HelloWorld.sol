ab7a8d499351c6d6e91c248bb24f3d276effe502
JaPilgrim <ethermeter@gmail.com>
2021-12-06 12:39:35 +0100

    Revert "Added Price to NFT"
    This reverts commit fa85dd6f90d9597de3bcba27f629e1c7bad71249.

--- blockchain/contracts/HelloWorld.sol
+++ blockchain/contracts/HelloWorld.sol
@@ -22,12 +22,10 @@ contract HelloWorld {
     uint256 public maxNumberOfNFTsToBuy;
 
     struct NFTOwnership {
-        address payable owner;
+        address owner;
         uint256 nftId;
         uint256 dropTime;
-        uint256 weiPrice;
         address reservedFor;
-
     }
 
     mapping(uint256 => NFTOwnership) public nftOwnerships;
@@ -43,12 +41,12 @@ contract HelloWorld {
     }
 
 
-    // This function lets a user create a drop by specifiyng a drop time, NFT Price in Wei (1 ETH = 1e18 Wei) and the number of available NFTs.
+    // This function lets a user create a drop by specifiyng a drop time and the number of available NFTs.
     // During the creation of the drop, the maximum number of NFTs a user can reserve/buy in this drop is calculated.
     // It is one for a total number of NFTs lower than 20 and 5% otherwise.
-    function createDrop(uint256 _dropTime, uint256 _weiPrice, uint256 _numberOfNFTS) public {
+    function createDrop(uint256 _dropTime, uint256 _numberOfNFTS) public {
         for (uint256 i = 0; i < _numberOfNFTS; i++) {
-            mockNFT(_dropTime, _weiPrice, i + 1);
+            mockNFT(_dropTime, i + 1);
         }
         // We need to let users buy 1 NFT instead of 5% if there are less than 20
         if (_numberOfNFTS < 20) {
@@ -59,14 +57,13 @@ contract HelloWorld {
     }
 
     // Create mock NFTs for drop by creating a list of hashes.
-    function mockNFT(uint256 _dropTime, uint256 _weiPrice, uint256 _number) internal {
+    function mockNFT(uint256 _dropTime, uint256 _number) internal {
         uint256 nftHash = generateRandomNumber(_number);
         NFTOwnership memory nftOwnership;
         nftOwnership.nftId = nftHash;
         nftOwnership.owner = nftOwner;
         nftOwnership.reservedFor = nftOwner;
         nftOwnership.dropTime = _dropTime;
-        nftOwnership.weiPrice = _weiPrice;
         nftOwnerships[nftHash] = nftOwnership;
         availableNFTs.push(nftHash);
         availableNFTsCount = availableNFTs.length;
@@ -123,9 +120,8 @@ contract HelloWorld {
 
 
     // This function lets a user buy her reserved NFTs (one at a time)
-    // Payment of WeiPrice is done to current owner
     //TODO: Think about a time span during which the reserved NFTs have to be bought 
-    function buyNFT(uint256 _nftHash) public payable {
+    function buyNFT(uint256 _price, uint256 _nftHash) public payable {
         require(
             nftOwnerships[_nftHash].dropTime <= block.timestamp,
             "Droptime not yet reached!"
@@ -138,13 +134,10 @@ contract HelloWorld {
             nftOwnerships[_nftHash].owner != msg.sender,
             "This NFT already belongs to you"
         );
-        require(
-            nftOwnerships[_nftHash].weiPrice <= msg.value,
-            "You have sent insufficient funds to buy the desired NFT"
-        );
         // string(abi.encodePacked("Drop has not started yet! ",((nftOwnerships[_nftHash].droptime-block.timestamp)/86400)," Days, ",((nftOwnerships[_nftHash].droptime-block.timestamp)/3600)," Hours,", ((nftOwnerships[_nftHash].droptime-block.timestamp)/60)," Minutes, and ", ((nftOwnerships[_nftHash].droptime-block.timestamp))," Seconds left.")));
-        nftOwnerships[_nftHash].owner.transfer(nftOwnerships[_nftHash].weiPrice);
-        nftOwnerships[_nftHash].owner = payable(msg.sender);
+        _price = msg.value;
+        nftOwner.transfer(_price);
+        nftOwnerships[_nftHash].owner = msg.sender;
     }
 
     // Helper function to created hashes
