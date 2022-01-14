40372cd5672e847802c9ae3afa3ff9b291581784
tuilagio <47163309+tuilagio@users.noreply.github.com>
2022-01-14 16:00:15 +0100

    135 test action annouce collection (#157)
    * add integration test for wallets and announcement collection
    
    * fixup: Format Python code with Black
    
    * .
    
    * support write collection id to file
    
    Co-authored-by: autoblack <felix.steinkohl@gmx.de>

--- backend/database-collection-schemas/createWalletsCollection.py
+++ backend/database-collection-schemas/createWalletsCollection.py
@@ -1,11 +1,43 @@
 import os
+import argparse
+import sys
 from appwrite.client import Client
 from appwrite.services.database import Database
 
-APPWRITE_ENDPOINT = os.environ.get("APPWRITE_ENDPOINT")
-APPWRITE_PROJECT = os.environ.get("APPWRITE_PROJECT")
-APPWRITE_API_KEY = os.environ.get("APPWRITE_API_KEY")
+parser = argparse.ArgumentParser("\n--apikey, --endpoint, --projectid \n")
 
+parser.add_argument(
+    "--apikey",
+    type=str,
+    default=None,
+    help="API key to access appwrite backend, you can instead "
+    "also set the environment variable APPWRITE_API_KEY",
+)
+parser.add_argument(
+    "--endpoint",
+    type=str,
+    default=None,
+    help="URL of the appwrite endpoint, you can instead "
+    "also set the environment variable APPWRITE_ENDPOINT",
+)
+parser.add_argument(
+    "--projectid",
+    type=str,
+    default=None,
+    help="Project ID of the targeted appwrite project, you can instead "
+    "also set the environment variable APPWRITE_PROJECT",
+)
+args = parser.parse_args()
+
+APPWRITE_ENDPOINT = (
+    str(args.endpoint) if args.endpoint else os.environ.get("APPWRITE_ENDPOINT")
+)
+APPWRITE_PROJECT = (
+    str(args.projectid) if args.projectid else os.environ.get("APPWRITE_PROJECT")
+)
+APPWRITE_API_KEY = (
+    str(args.apikey) if args.apikey else os.environ.get("APPWRITE_API_KEY")
+)
 
 client = Client()
 database = Database(client)
@@ -16,26 +48,35 @@ database = Database(client)
     .set_key(APPWRITE_API_KEY)
 )
 
-createCollectionResult = database.create_collection(
-    "Wallets",
-    ["role:member"],
-    [],
-    [
-        {
-            "label": "User Id",
-            "key": "userId",
-            "type": "text",
-            "required": True,
-            "array": False,
-        },
-        {
-            "label": "Wallet Address",
-            "key": "walletAddress",
-            "type": "text",
-            "required": True,
-            "array": False,
-        },
-    ],
-)
+try:
+    createCollectionResult = database.create_collection(
+        "Wallets",
+        ["role:member"],
+        [],
+        [
+            {
+                "label": "User Id",
+                "key": "userId",
+                "type": "text",
+                "required": True,
+                "array": False,
+            },
+            {
+                "label": "Wallet Address",
+                "key": "walletAddress",
+                "type": "text",
+                "required": True,
+                "array": False,
+            },
+        ],
+    )
 
-print(createCollectionResult)
+    print(createCollectionResult)
+    print(createCollectionResult["$id"])
+    with open("walletID.txt", "w") as f:
+        print(createCollectionResult["$id"], file=f)
+except Exception as e:
+    print(e)
+    sys.exit(1)
+
+sys.exit(0)
