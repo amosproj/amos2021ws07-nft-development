7d64847a728ac86bf90f82613161f073c790ce19
dominic1608 <mail@d-heil.de>
2022-01-26 02:12:57 +0100

    Added first version of adding users to contract team

--- frontend/src/components/AddUserToContractPartnerTeam.js
+++ frontend/src/components/AddUserToContractPartnerTeam.js
@@ -5,10 +5,13 @@ import {
 	Box,
 	Button,
 } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import ParagraphTypography from "./ParagraphTypography";
+import { ConnectWalletButton } from "../pages/Profile";
+import ethereumContractApi from "../api/ethereumContractApi";
+import ConditionalAlert from "./ConditionalAlert";
 
 /**
  * Component that can be used by admins to invite or remove users to a user team.
@@ -17,7 +20,14 @@ import ParagraphTypography from "./ParagraphTypography";
  * @returns {JSX.Element}
  */
 export default function AddUserToContractPartnerTeam({ teamToEdit }) {
-	// const [metaMaskIsConnected, setMetaMaskIsConnected] = useState(false);
+	const [metaMaskIsConnected, setMetaMaskIsConnected] = useState(false);
+	const [connectedToMetaMaskMessage, setConnectedToMetaMaskMessage] = useState("");
+
+	useEffect(() => {
+		if (ethereumContractApi.selectedAccount !== null) {
+			setMetaMaskIsConnected(true);
+		}
+	}, []);
 
 	const addUserToContractTeam = async (event) => {
 		event.preventDefault();
@@ -25,9 +35,36 @@ export default function AddUserToContractPartnerTeam({ teamToEdit }) {
 		// const email = data.get("addAddress");
 	};
 
+	const connectMetaMask = () => ethereumContractApi.init().then(() => {
+		setMetaMaskIsConnected(true);
+		setConnectedToMetaMaskMessage("Successfully connected to MetaMask.");
+	});
+
+	if (!metaMaskIsConnected) {
+		return (
+			<Grid container spacing={2} alignItems="center" justifyContent="center" direction="column">
+				<Grid item style={{ width: "100%" }}>
+					<Grid container justify="space-between" style={{ width: "100%" }}>
+						<ParagraphTypography variant="body1" align="left" gutterBottom>
+							Connect to Metamask in order to add or remove ETH addresses to team {teamToEdit}.
+						</ParagraphTypography>
+					</Grid>
+					<Grid item style={{ marginTop: "20px", marginBottom: "10px" }}>
+						<Grid container alignItems="center" justifyContent="center">
+							<ConnectWalletButton onClick={connectMetaMask}/>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		);
+	}
+
 	return <>
 		<Grid container spacing={2} alignItems="center" justifyContent="center" direction="column">
 			<Grid item style={{ width: "100%" }}>
+				<Grid item  style={{ width: "100%", marginBottom: connectedToMetaMaskMessage === "" ? 0 : "15px" }}>
+					<ConditionalAlert severity="info" text={connectedToMetaMaskMessage} />
+				</Grid>
 				<Grid container justify="space-between" style={{ width: "100%" }}>
 					<ParagraphTypography variant="body1" align="left" gutterBottom>
 						Add an ETH address to the {teamToEdit} in the main NFT contract. This will allow them to create a new drop.
